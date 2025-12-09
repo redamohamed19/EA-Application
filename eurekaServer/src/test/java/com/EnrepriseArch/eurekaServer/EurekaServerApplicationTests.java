7358a6280964ea8bca4072451592a03a4ea92151
sayah-reda <reda1998.rs@gmail.com>
2025-12-09 12:04:19 +0100

    feat: add Spring Cloud config service & Eureka server

---
+++ eurekaServer/src/test/java/com/EnrepriseArch/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EnrepriseArch.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
